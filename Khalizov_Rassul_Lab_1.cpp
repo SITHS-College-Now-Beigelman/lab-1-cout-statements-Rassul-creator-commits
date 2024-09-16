73a8d07daccbe3f95f638a2f6c112d345c9bf380
Rassul-creator-commits <181149570+Rassul-creator-commits@users.noreply.github.com>
2024-09-16 15:13:52 -0400

    Add files via upload

---
+++ Khalizov_Rassul_Lab_1.cpp
@@ -0,0 +1,41 @@
+// Rassul Lab 1 9/10/24 
+
+#include <iostream>
+
+using namespace std;
+
+//the begining of the code 
+
+int main()
+{
+    // the inputs create a box of hashtags around the given writing 
+    cout << "######################################################################################" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "# Ways to access the Task Manager on your Windows computer:                          #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "#            Press the key combination Ctrl + Shift + Escape                         #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "#            Press the key combination Ctrl + Alt + Delete and select “Task Manager  #" << endl; 
+    cout << "#                                                                                    #" << endl;
+    cout << "#            Type “Task Manager” in the Windows Start menu search                    #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "######################################################################################" << endl;  
+    
+    return 0;
+}
+
+/*[Running] cd "c:\Users\college\Documents\GitHub\lab-1-cout-statements-Rassul-creator\" && g++ Khalizov_Rassul_Lab_1.cpp -o Khalizov_Rassul_Lab_1 && "c:\Users\college\Documents\GitHub\lab-1-cout-statements-Rassul-creator\"Khalizov_Rassul_Lab_1
+######################################################################################
+#                                                                                    #
+# Ways to access the Task Manager on your Windows computer:                          #
+#                                                                                    #
+#            Press the key combination Ctrl + Shift + Escape                         #
+#                                                                                    #
+#            Press the key combination Ctrl + Alt + Delete and select “Task Manager  #
+#                                                                                    #
+#            Type “Task Manager” in the Windows Start menu search                    #
+#                                                                                    #
+######################################################################################
+
+[Done] exited with code=0 in 1.485 seconds
+*/
